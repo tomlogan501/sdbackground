2796e78f1c537c05a964d02cf893e30ff99fc142
Thomas SIMON <thomas.simon@ensta-paris.fr>
2019-11-22 17:03:11 +0100

    Global package management Source edit

--- src/sdbackground_node.cpp
+++ src/sdbackground_node.cpp
@@ -1,12 +1,29 @@
+
+/*
+ * ROS
+ */
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+
+/*
+ * EXTERN
+ */
+#include "extern/def.h"
+#include "extern/nrutil.h"
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   // ...
 }
 
-ros::NodeHandle nh;
-image_transport::ImageTransport it(nh);
-image_transport::Subscriber sub = it.subscribe("in_image_base_topic", 1, imageCallback);
-image_transport::Publisher pub = it.advertise("out_image_base_topic", 1);
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "sdbackground");
+  ros::NodeHandle nh;
+
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("in_image_topic", 1, imageCallback);
+  image_transport::Publisher pub = it.advertise("out_image_topic", 1);
+
+  ros::spin();
+}
